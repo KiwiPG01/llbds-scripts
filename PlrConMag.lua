2f30356582edbab6e343f23a41e58dc40ac48a0f
KiwiPG01 <71287511+KiwiPG01@users.noreply.github.com>
2023-06-13 10:38:11 -0600

    Create PlrConMag.lua

---
+++ PlrConMag.lua
@@ -0,0 +1,10 @@
+ll.registerPlugin("PlrConMag", "A lua plugin that tracks player connection to the server.", {0,0,1, dev}, "KiwiPG")
+-- Player Connection Manager --
+-- A lua plugin that tracks player connection to the server. --
+-- By: KiwiPG --
+
+
+--Server side--
+local DBS = "" -- Insert where you will store the player data
+
+local function PlrJoined = 
